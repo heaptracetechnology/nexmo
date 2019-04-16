3e89207e8caad83955f8f4919dde7f38f3696e59
ht68rohit <rohits@heaptrace.com>
2019-04-16 17:43:31 +0530

    updated send message

--- messaging/message_test.go
+++ messaging/message_test.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"net/http/httptest"
-	"os"
 )
 
 type SendSMS struct {
@@ -64,35 +63,6 @@ var _ = Describe("Send SMS Negative", func() {
 		Context("send", func() {
 			It("Should result http.StatusBadRequest", func() {
 				Expect(recorder.Code).To(Equal(http.StatusBadRequest))
-			})
-		})
-	})
-})
-
-var _ = Describe("Receive SMS", func() {
-
-	os.Setenv("API_KEY", "26ee1c8b")
-	os.Setenv("API_SECRET", "gGJmYyeEh5Po05f2")
-
-	sms := SendSMS{From: "9876543210", To: "0123456789", Text: "Testing SMS"}
-	requestBody := new(bytes.Buffer)
-	errr := json.NewEncoder(requestBody).Encode(sms)
-	if errr != nil {
-		log.Fatal(errr)
-	}
-
-	request, err := http.NewRequest("POST", "/receive", requestBody)
-	if err != nil {
-		log.Fatal(err)
-	}
-	recorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(Receiver)
-	handler.ServeHTTP(recorder, request)
-
-	Describe("Receive SMS message", func() {
-		Context("receive", func() {
-			It("Should result http.StatusOK", func() {
-				Expect(recorder.Code).To(Equal(http.StatusOK))
 			})
 		})
 	})
